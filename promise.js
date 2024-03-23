fc5bd361538fec2907e88c26d4e273c49cc63c1b
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-03-23 17:33:45 -0400

    update

--- promise.js
+++ promise.js
@@ -22,3 +22,15 @@ promise.then(()=>{
 })
 
 console.log(4)
+
+// promise 中值的穿透是什么？
+
+/**
+ * .then, .catch 的期望是函数，如果传入非函数则会发生值穿透
+ * 如果传入非函数，then返回promise的data会返回上一个promise的data，并返回resolve
+ */
+
+Promise.resolve(1)
+.then(2)
+.then(Promise.resolve(3))
+.then(console.log)
